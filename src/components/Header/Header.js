f3523f2a8ef76e3132a26ef424d8d0ceba123cec
An Doan <dtan42@gmail.com>
2020-10-23 09:53:16 +1000

    set up project

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+function Header() {
+    return (
+        <div className='header'>
+            <div className="header__left">
+                <img src="" alt="" />
+                <h3>PTE Intensive</h3>
+            </div>
+            <div className="header__right">
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
